9a8dbb60e371e0e215c76042ccccaea9a3a38f12
aiyus <aiyusboss@gmail.com>
2023-06-08 17:33:14 +0100

    last commit

--- express.js
+++ express.js
@@ -15,14 +15,9 @@ app.get("/api/course", (req, res) => {
 });
 
 app.post("/api/course", (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  let result = Joi.validate(req.body, schema);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(details[0].message);
+
   let course = {
     id: courses.length + 1,
     name: req.body.name,
@@ -33,16 +28,10 @@ app.post("/api/course", (req, res) => {
 
 app.put("/api/course/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("the course is not valid");
+  if (!course) return res.status(404).send("the course is not valid");
 
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  let result = Joi.validate(req.body, schema);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(details[0].message);
 
   course.name = req.body.name;
   res.send(course);
@@ -50,10 +39,26 @@ app.put("/api/course/:id", (req, res) => {
 
 app.get("/api/course/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("the course is not valid");
+  if (!course) return res.status(404).send("the course is not valid");
   res.send(course);
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port 3000.. or ${port}`);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(course, schema);
+}
+
+app.delete("/api/course/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("the course is not valid");
+
+  let index = courses.indexOf(course);
+  courses.splice(index);
+  res.send(course);
 });
