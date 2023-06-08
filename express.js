9df98aae6ddac6dc3a65fa7ccca6fe9bf6a40f5e
aiyus <aiyusboss@gmail.com>
2023-06-08 17:02:46 +0100

    another commit

--- express.js
+++ express.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -14,11 +15,42 @@ app.get("/api/course", (req, res) => {
 });
 
 app.post("/api/course", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  let result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
   let course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/course/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course is not valid");
+
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  let result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.get("/api/course/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course is not valid");
   res.send(course);
 });
 let port = process.env.PORT || 3000;
